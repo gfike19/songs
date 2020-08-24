9594f90290c7590ec84e79b32f358367355cfd61
gfike19 <gfike19@gmail.com>
2020-08-24 14:16:18 -0500

    finished most of 235

--- src/components/songList.js
+++ src/components/songList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {selectSong} from '../actions';
 
 class SongList extends Component {
     renderList() {
@@ -7,7 +8,10 @@ class SongList extends Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className=" ui button primary">Select</button>
+                        <button 
+                        className=" ui button primary"
+                        onClick={() => this.props.selectSong(song)}>
+                            Select</button>
                     </div>
                     <div className="content">{song.title}</div>
                 </div>
@@ -17,7 +21,6 @@ class SongList extends Component {
 
     render () {
         // this.props with mapToProps and connect will have value of songs: state.songs
-        
         return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
@@ -27,6 +30,8 @@ const mapStateToProps = (state) => {
     return {songs: state.songs};
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+    selectSong: selectSong
+})(SongList);
 // above syntax is because connect is function inception
 // connect returns a function and THAT function returns a value
